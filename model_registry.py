22950061202dda3dcb353fd95166bcc7b76182b6
Gospodinvin <158985517+Gospodinvin@users.noreply.github.com>
2025-12-30 14:58:38 +0500

    Update model_registry.py

--- model_registry.py
+++ model_registry.py
@@ -1,12 +1,13 @@
 # model_registry.py
 from model import CandleModel
 
+# Создаём модели при старте
 MODELS = {
-    "1": CandleModel(),
-    "2": CandleModel(),
-    "5": CandleModel(),
-    "10": CandleModel(),
+    "1": CandleModel("1"),
+    "2": CandleModel("2"),
+    "5": CandleModel("5"),
+    "10": CandleModel("10"),
 }
 
 def get_model(tf: str):
-    return MODELS.get(tf, MODELS["1"])  # fallback на 1
+    return MODELS.get(tf, MODELS["1"])
